8e8689811a3911290d1c7acb4068a66a71fe20d5
jwz <3544346335@qq.com>
2023-11-27 17:36:31 -0500

    exercise 4

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -4,6 +4,26 @@
 #[cfg(feature = "axstd")]
 use axstd::println;
 const PLASH_START: usize = 0x22000000;
+
+const SYS_HELLO: usize = 1;
+const SYS_PUTCHAR: usize = 2;
+const SYS_TERMINATE: usize = 3;
+static mut ABI_TABLE: [usize; 16] = [0; 16];
+fn register_abi(num: usize, handle: usize) {
+  unsafe { ABI_TABLE[num] = handle; }
+}
+fn abi_hello() {
+  println!("[ABI:Hello] Hello, Apps!");
+}
+fn abi_putchar(c: char) {
+  println!("[ABI:Print] {c}");
+}
+fn abi_terminate()
+{
+  println!("teminate");
+  axstd::process::exit(1);
+  //arceos_api::sys::ax_terminate();
+}
 #[cfg_attr(feature = "axstd", no_mangle)]
 fn main() { 
   let load_start = PLASH_START as *const u8;
@@ -24,12 +44,28 @@ load_code.as_ptr());
     println!("run code {:?}; address [{:?}]", run_code,
   run_code.as_ptr());
     println!("Load payload ok!");
+
+
+    register_abi(SYS_HELLO, abi_hello as usize);
+    register_abi(SYS_PUTCHAR, abi_putchar as usize);
+    register_abi(SYS_TERMINATE, abi_terminate as usize);
     println!("Execute app ...");
+      let arg0: u8 = b'A';
 // execute app
-  unsafe { core::arch::asm!("
-   li   t2, {run_start}
-   jalr  t2
-   j    .",
-    run_start = const RUN_START,
- )}
+      unsafe { core::arch::asm!("
+       li   t0, {abi_num}
+       slli  t0, t0, 3
+       la   t1, {abi_table}
+       add  t1, t1, t0
+       ld   t1, (t1)
+       jalr  t1
+       li   t2, {run_start}
+       jalr  t2
+       j   .",
+        run_start = const RUN_START,
+        abi_table = sym ABI_TABLE,
+//abi_num = const SYS_HELLO,
+        abi_num = const SYS_TERMINATE,
+        in("a0") arg0,
+     )}
 }
